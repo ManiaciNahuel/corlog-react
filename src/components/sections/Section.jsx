f800ba6ec2ce9e01ae7b71e0d2114da08066d25f
Nahuel Maniaci <nahuelmaniaci@gmail.com>
2024-02-28 10:54:06 -0300

    Base commit

---
+++ src/components/sections/Section.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+export const Section = () => {
+  return (
+    <section id="section" className="about sec-pad">
+      {/*  <!-- SECTION --> */}
+      <div className="main-container">
+        <h2 className="heading heading-sec heading-sec__mb-med">
+          <span className="heading-sec__main">Subtitulo 1 </span>
+        </h2>
+        <div className="about__content">
+          <div className="about__content-main">
+            <h3 className="about__content-title">Subtitulo 2</h3>
+            <div className="about__content-details">
+              <p className="about__content-details-para">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, explicabo quae? Eveniet omnis fugiat doloribus laudantium voluptates ab ipsum dolores architecto itaque? Accusamus voluptas, reiciendis rem consequuntur ab explicabo dignissimos!
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas pariatur consequatur expedita voluptate asperiores earum facilis velit dolor laboriosam dolorum suscipit, non et ratione quibusdam aperiam ad iste nulla veritatis.
+                Expedita doloribus voluptates pariatur, harum reprehenderit sequi sed, veniam dolore magnam dolores nam vel vero obcaecati nulla asperiores atque sunt est maxime at, ea dolorem dignissimos illo consequuntur! Nesciunt, dolore!
+                Qui nemo dolorem eius doloremque quaerat maxime beatae, ipsa amet odit cupiditate reiciendis vero eaque? Reprehenderit ipsam deserunt sed laborum alias laboriosam dolorem, odio quibusdam in reiciendis corporis voluptatum molestiae.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi voluptatem delectus vitae reiciendis, aut repellat neque alias libero quo blanditiis doloremque facere numquam eum consequuntur dolores ullam nam ut voluptatibus!
+                Molestiae minima magni corporis ea, perspiciatis quisquam qui tenetur quod delectus doloremque! Quas dolor soluta velit ea assumenda eaque distinctio sit est nisi. Vitae amet id, excepturi impedit sapiente laudantium?
+                Labore totam laborum in! Fuga adipisci blanditiis magni debitis, velit amet! Nostrum optio molestiae cum. Sapiente molestiae similique, facere molestias itaque vero, repudiandae numquam velit perferendis illo libero nulla totam?
+                Atque veritatis distinctio fugit eligendi ratione reiciendis odio nobis repudiandae laudantium dolor blanditiis velit tempora recusandae amet, voluptatem in ducimus tenetur excepturi? Id, nisi. Amet quam mollitia et atque! Eaque.S
+              </p>
+            </div>
+       
+          </div>
+          <div className="about__content-skills">
+            <h3 className="about__content-title">Subtitulo 2</h3>
+            <p className="about__content-details-para">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, libero! Excepturi possimus error earum fugiat quo recusandae tempore perspiciatis! Id ad ipsa neque necessitatibus laudantium at esse expedita corrupti doloremque.
+              Nam, nobis earum magnam et ipsa voluptates quibusdam molestias quaerat sint! Mollitia molestias alias maxime quibusdam nisi aut delectus ipsam! Quod beatae aspernatur totam blanditiis odio aut doloremque, consequatur nostrum.
+              Ratione hic molestias, blanditiis vel voluptatibus explicabo beatae, unde rem mollitia enim et quisquam numquam modi sapiente deleniti ducimus est officiis excepturi minima, consequatur id. Ducimus doloremque expedita mollitia earum.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
