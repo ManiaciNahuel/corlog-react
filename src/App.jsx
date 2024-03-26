b0b105ddcf1d61ae3ba76b1f87594a6a986001a1
Nahuel Maniaci <nahuelmaniaci@gmail.com>
2024-03-25 21:26:24 -0300

    NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,20 @@ import { Footer } from "./components/navigation/Footer";
 import { NavBar } from "./components/navigation/NavBar";
 import { Section } from "./components/sections/Section";
 import { Home } from "./components/sections/Home";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <NavBar />
-      <Home />
-      <Section />
-      <Footer />
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/section" element={<Section />}/>
+
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </>
   );
 }
--- src/components/navigation/NavBar.jsx
+++ src/components/navigation/NavBar.jsx
@@ -1,8 +1,14 @@
 import React, { useState } from "react";
-import logo from "../../assets/png/sample-logo-white.png";
+import logo from "../../assets/imgs/header/logo.png";
+import logotipo from "../../assets/imgs/header/logotipo.png";
+
+import whatsapp from "../../assets/imgs/header/wapp.png";
+import facebook from "../../assets/imgs/header/facebook.png";
+import gmail from "../../assets/imgs/header/gmail.png";
+import isntagram from "../../assets/imgs/header/instagram.png";
+
 import hamMenu from "../../assets/svg/ham-menu.svg";
 import hamMenuClose from "../../assets/svg/ham-menu-close.svg";
-
 
 export const NavBar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -12,11 +18,7 @@ export const NavBar = () => {
       <div className="header__content">
         <div className="header__logo-container">
           <a href="/">
-            <img
-              src={logo}
-              alt="Logo"
-              className="header__logo-img"
-            />
+            <img src={logo} alt="Logo" className="header__logo-img" />
           </a>
         </div>
         <div className="header__main">
@@ -28,20 +30,32 @@ export const NavBar = () => {
               </a>
             </li>
             <li className="header__link-wrapper">
-              <a href="./#section" className="header__link">
-                Section 1{" "}
+              <a href="./#" className="header__link">
+                Seguir envio
               </a>
             </li>
             <li className="header__link-wrapper">
               <a href="./#" className="header__link">
-                Section 2
+                Cotizar envio
+              </a>
+            </li>
+            <li className="header__link-wrapper">
+              <a href="./#section" className="header__link">
+                Destinos{" "}
+              </a>
+            </li>
+            <li className="header__link-wrapper">
+              <a href="./#" className="header__link">
+                Ayuda
               </a>
             </li>
           </ul>
           {/* Hamburger menu */}
           <div className="header__main-ham-menu-cont">
-            <button onClick={() => setIsOpen(!isOpen)}
-              className={`${isOpen ? 'btn-open' : ''}`}>
+            <button
+              onClick={() => setIsOpen(!isOpen)}
+              className={`${isOpen ? "btn-open" : ""}`}
+            >
               <img
                 src={isOpen ? hamMenuClose : hamMenu}
                 alt="hamburger menu"
@@ -50,28 +64,113 @@ export const NavBar = () => {
             </button>
           </div>
         </div>
+        <div className="header__socials">
+          <ul>
+            <li>
+              <a
+                href="https://www.instagram.com/corlogargentina/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {" "}
+                <img src={isntagram} alt="" />
+              </a>
+            </li>
+            <li>
+              <a
+                class="wa1"
+                href="https://web.whatsapp.com/send?phone=+5493517335050&text=Hola%20Corlog!%20Quiero%20hacer%20una%20consulta..."
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src={whatsapp} alt="logoWhatsapp" />
+              </a>
+              {/* <a class="wa2"
+                        href="https://api.whatsapp.com/send?phone=+5493517335050&text=Hola%20Corlog!%20Quiero%20hacer%20una%20consulta..."
+                        target="_blank" rel="noopener noreferrer"><img src={whatsapp} alt="logoWhatsapp" /></a> */}
+            </li>
+            <li>
+              <a
+                href="https://www.facebook.com/corlogargentina"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {" "}
+                <img src={facebook} alt="" />
+              </a>
+            </li>
+            <li>
+              <a
+                href="mailto:corlogargentina@gmail.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src={gmail} alt="" />
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
       {isOpen && (
+        <>
+        <div className="header__sm-menu-overlay" onClick={() => setIsOpen(!isOpen)}></div>
         <div className="header__sm-menu">
           <div className="header__sm-menu-content">
-            <ul className="header__sm-menu-links">
-              <li className="header__sm-menu-link">
-                <a href="./"> Inicio </a>
+          <img src={logotipo} alt="logo" className="header__sm-menu-logo"/>
+            
+            <ul>
+              <li>
+                <a href="../index.html" className="header__sm-menu-item">
+                  Inicio
+                </a>
               </li>
-
-              <li className="header__sm-menu-link">
-                <a href="./#section"> Section 1 </a>
+              <li>
+                <a href="../pages/seguimiento.html" className="header__sm-menu-item">
+                  Seguir envío
+                </a>
               </li>
-
-              <li className="header__sm-menu-link">
-                <a href="./"> Section 2 </a>
+              <li>
+                {" "}
+                <a href="../pages/cotizacion.html" className="header__sm-menu-item">
+                  Cotizar envío
+                </a>
               </li>
-
+              <li>
+                <a href="../pages/destinos.html" className="header__sm-menu-item">
+                  Destinos
+                </a>
+              </li>
+              <li>
+                <a href="../pages/destinos.html" className="header__sm-menu-item">
+                  Nuestras sucursales
+                </a>
+              </li>
+              <li>
+                <a href="../pages/ayuda.html" className="header__sm-menu-item">
+                  Ayuda
+                </a>
+              </li>
+              <li>
+                {" "}
+                <a href="../pages/nosotros.html" className="header__sm-menu-item">
+                  Nosotros
+                </a>
+              </li>
+              <li>
+                <a href="../pages/nosotros.html" className="header__sm-menu-item">
+                  Servicios
+                </a>
+              </li>
+              <li>
+                <a href="../pages/ayuda.html" className="header__sm-menu-item">
+                  Redes
+                </a>
+              </li>
             </ul>
           </div>
         </div>
+        </>
       )}
-
     </header>
   );
 };
